3b122f9cd5f299043125648f4f6245a947762d09
Ana Esteves <11687547+profana@users.noreply.github.com>
2023-10-15 22:23:35 +0100

    Criar classearitmetica e adicionar método

---
+++ Aritmetica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    internal class Aritmetica
+	//implementa operações aritméticas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicação Cálculos");
+
+            Console.ReadKey();
+        }
+    }
+}
